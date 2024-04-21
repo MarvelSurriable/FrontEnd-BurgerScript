04f3faadcb7c3e256b4df65225d7c43a0d2b899e
LeanBader <baderleandro@gmail.com>
2024-04-21 01:27:23 -0300

    Se creo la funcion de editar y eliminar productos

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import CrearProductos from "./Components/Sections/CrearProductos"
 import Navbar from "./Components/Navbar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import Home from "./Components/Pages/Home/Home"
 import Administracion from "./Components/Pages/Administracion";
 import Contacto from "./Components/Pages/Contacto";
 import Footer from "./Components/Footer";
 import Error404 from "./Components/Pages/Error404/Error404";
+import Editar from "./Components/Sections/Editar";
 
 function App() {
   return (
@@ -22,6 +22,7 @@ function App() {
             <Route path="/contacto" element={<Contacto />} />
             <Route path="/crear-producto" element={<CrearProductos></CrearProductos>}></Route>
             <Route path="/error" element={<Error404/>}></Route>
+            <Route path="/editar/:id" element={<Editar></Editar>}></Route>
           </Routes>
         </main>
         <footer>
--- src/Components/Sections/Producto.jsx
+++ src/Components/Sections/Producto.jsx
@@ -1,8 +1,11 @@
 
 import { Button } from "react-bootstrap";
 import Image from 'react-bootstrap/Image';
+import { useNavigate } from "react-router-dom";
+import EliminarProducto from "./EliminarProducto";
 
-const Producto = ({producto}) => {
+const Producto = ({producto, getProductos}) => {
+  const navigate = useNavigate();
   return (
     <>
       <tr>
@@ -10,11 +13,13 @@ const Producto = ({producto}) => {
         <td>{producto.title}</td>
         <td>{producto.category}</td>
         <td>{producto.stock}</td>
-        <td><img src={producto.urlImg} alt="imagen" width="50px"/></td>
-        {/* <td><a href="${producto.urlImg}" target ="_blank" title="Ver imagen"><img src="${element.urlImg}" alt="Miniatura imagen producto" width=50px></a></td> */}
+        {/* <td><img src={producto.urlImg} alt="imagen" width="50px"/></td> */}
+        <td>{producto.controlStock}</td>
         <td className="d-flex justify-content-araund">
-            <Button type="button" variant="warning" className="mx-3">Editar</Button>
-            <Button type="button" variant="danger" className="mx-3">Eliminar</Button>
+            <Button type="button" variant="warning" className="mx-3" onClick={()=>{
+              navigate(`/editar/${producto.id}`)
+            }}>Editar</Button>
+            <EliminarProducto id={producto.id} getProductos={getProductos}></EliminarProducto>
         </td>
       </tr>
     </>
