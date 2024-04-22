518b0172cced572878a568991c066ffa3ba962de
AndiSaraza <andreamsaraza@gmail.com>
2024-04-22 01:50:14 -0300

    se maqueta y agrega validaciones a la pagina de contacto

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from "./Components/Navbar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Components/pages/Home/Home"
 import Administracion from "./Components/pages/Administracion";
-import Contacto from "./Components/pages/Contacto";
+import Contacto from "./Components/pages/Contacto/Contacto";
 import Footer from "./Components/Footer";
 import Error404 from "./Components/pages/Error404/Error404";
 import Editar from "./Components/Sections/Editar";
@@ -19,7 +19,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />            
             <Route path="/administracion" element={<Administracion />} />            
-            <Route path="/contacto" element={<Contacto />} />
+            <Route path="/contacto" element={<Contacto />}></Route>
             <Route path="/crear-producto" element={<CrearProductos></CrearProductos>}></Route>
             <Route path="/error" element={<Error404/>}></Route>
             <Route path="/editar/:id" element={<Editar></Editar>}></Route>
