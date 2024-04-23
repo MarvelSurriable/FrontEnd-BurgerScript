cf2630b8df7c61900265369aff606819f3c42dd3
AndiSaraza <andreamsaraza@gmail.com>
2024-04-23 18:19:51 -0300

    Se modifica estilos de la pagina contacto

--- src/Components/Pages/Contacto/Contacto.jsx
+++ src/Components/Pages/Contacto/Contacto.jsx
@@ -44,7 +44,7 @@ const Contacto = () => {
                   <Col xs={12} md={6}>
                     <Form>
                         <Form.Group controlId="nombre" className="mb-3">
-                            <Form.Label>Ingresa tu nombre</Form.Label>
+                            <Form.Label>Nombre y apellido</Form.Label>
                             <Form.Control type="text" maxLength={40} placeholder='Ingresa tu nombre' 
               name="nombre"
               {...formik.getFieldProps("nombre")}
@@ -65,7 +65,7 @@ const Contacto = () => {
             </Form.Group> 
                        
                         <Form.Group className="mb-3" controlId="email">
-                            <Form.Label>Ingresa tu mail</Form.Label>
+                            <Form.Label>Mail</Form.Label>
                             <Form.Control type="email" maxLength={50} placeholder='Ingresa tu email' 
               name="email"
               {...formik.getFieldProps("email")}
@@ -85,7 +85,7 @@ const Contacto = () => {
                )}
                         </Form.Group>
                         <Form.Group controlId="descrption">
-                            <Form.Label>Ingresa tu consulta</Form.Label>
+                            <Form.Label>Tu consulta</Form.Label>
                             <Form.Control as="textarea" rows={3}  maxLength={300} placeholder='Ingresa tu consulta' 
               name="description"
               {...formik.getFieldProps("description")}
@@ -104,7 +104,7 @@ const Contacto = () => {
                  </div>
                )}
                         </Form.Group>
-                    <div style={{ textAlign: 'right' }}>
+                    <div style={{ textAlign: 'center' }}>
                       <Button variant="light" type='submit' className='mt-2' >Enviar!</Button>    
                     </div>    
                     </Form>
@@ -115,7 +115,7 @@ const Contacto = () => {
                 <iframe
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3560.102370777457!2d-65.2093904844119!3d-26.836696096502912!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94225d3ad7f30f1d%3A0xf8606cd659b8e3e4!2sRollingCode%20School!5e0!3m2!1ses-419!2sar!4v1631390004391!5m2!1ses-419!2sar"
                 height="450"
-                className="w-100 p-4 "
+                className="w-100 py-4 "
                 allowFullScreen
                 style={{ borderRadius: '50px'}}
               ></iframe>
