2adec26fec9206e184cef043e831f2dcb40ea73d
Leandro Bader <baderleandro@gmail.com>
2024-04-30 16:54:57 -0300

    Se cambio la ruta de administracion

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import CrearProductos from "./Components/Sections/CrearProductos"
 import Navbar from "./Components/Navbar";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./Components/pages/Home/Home"
-import Administracion from "./Components/pages/Administracion";
+import Administracion from "./Components/pages/AdministracionBS";
 import Contacto from "./Components/pages/Contacto/Contacto";
 import Nosotros from "./Components/pages/AcercaDeNosotros/Nosotros";
 import Footer from "./Components/Footer";
